1cf28bd65baab5b7daaa3eb0e31e7a15ba8928c2
Hyunseok Cho <hoyajigi@gmail.com>
2013-11-11 18:19:53 +0900

    ,

--- julia.c
+++ julia.c
@@ -1,19 +1,22 @@
+/*
+ * OpenCL Boilerplate by Hyunseok Cho (i@hoyajigi.com)
+ * First created at 2013.10.04
+ * All Rights Reserved
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <time.h>
 #include <math.h>
+#include <CL/cl.h>
+#include "cl_util.h"
 #include "timers.h"
 
+#define ERROR(err) fprintf(stderr, "[%s:%d] ERROR: %s\n",__FILE__,__LINE__,err);exit(EXIT_FAILURE);
 #define COUNT_MAX   2000
 #define MIN(x,y)    ((x) < (y) ? (x) : (y))
-
-
-typedef struct {
-  unsigned int r;
-  unsigned int g;
-  unsigned int b;
-} RgbColor;
-
-RgbColor HSVtoRGB(unsigned h, unsigned s, unsigned v);
 
 
 // Main part of the below code is originated from Lode Vandevenne's code.
@@ -44,11 +47,35 @@ void julia(int w, int h) {
   cRe = -0.7;
   cIm = 0.27015;
 
-  int (*r)[h] = (int (*)[h])calloc(w * h, sizeof(int));
-  int (*g)[h] = (int (*)[h])calloc(w * h, sizeof(int));
-  int (*b)[h] = (int (*)[h])calloc(w * h, sizeof(int));
+  /* Host data structures */
+  cl_platform_id   *platforms;
+  cl_uint          num_platforms;
+  cl_device_type   dev_type = CL_DEVICE_TYPE_DEFAULT;
+  cl_device_id     dev;
+  cl_context       context;
+  // NOTE : You might have multiple cmd_queue but whatever
+  cl_command_queue cmd_queue;
+  cl_program       program;
+  cl_kernel        kernel;
+  // TODO : define your variables
+  cl_mem           R,G,B;
+  cl_int           err;
+  cl_uint          num_dev = 0;
+  cl_event         ev_bp;
+  
+  // TODO : 
+/*
+    // loop through every pixel
+  for (int y = 0; y < h; y++)
+  {
+    for (int x = 0; x < w; x++)
+*/
+  size_t lws[2]={16,16};
+  size_t gws[2]={h/16,w/16};
 
-  printf( "  Sequential C version\n" );
+
+
+  printf( "  Parallel OpenCL version\n" );
   printf( "\n" );
   printf( "  Create an ASCII PPM image of the Julia set.\n" );
   printf( "\n" );
@@ -58,44 +85,151 @@ void julia(int w, int h) {
 
   timer_init();
   timer_start(0);
-
-  // loop through every pixel
-  for (int y = 0; y < h; y++)
-  {
-    for (int x = 0; x < w; x++)
-    {
-      // calculate the initial real and imaginary part of z,
-      // based on the pixel location and zoom and position values
-      newRe = 1.5 * (x - w / 2) / (0.5 * zoom * w) + moveX;
-      newIm = (y - h / 2) / (0.5 * zoom * h) + moveY;
-
-      // i will represent the number of iterations
-      int i;
-      //start the iteration process
-      for (i = 0; i < maxIterations; i++)
-      {
-        // remember value of previous iteration
-        oldRe = newRe;
-        oldIm = newIm;
-
-        // the actual iteration, the real and imaginary part are calculated
-        newRe = oldRe * oldRe - oldIm * oldIm + cRe;
-        newIm = 2 * oldRe * oldIm + cIm;
-
-        // if the point is outside the circle with radius 2: stop
-        if ((newRe * newRe + newIm * newIm) > 4) break;
-      }
-
-      // use color model conversion to get rainbow palette, 
-      // make brightness black if maxIterations reached
-      RgbColor color = HSVtoRGB(i % 256, 255, 255 * (i < maxIterations));
-      r[y][x] = color.r;
-      g[y][x] = color.g;
-      b[y][x] = color.b;     
-    }
+  // Platform
+  err = clGetPlatformIDs(0, NULL, &num_platforms);
+  CHECK_ERROR(err);
+  if(num_platforms == 0) {
+    ERROR("No OpenCl platform");
   }
+  printf("Number of platforms: %u\n",num_platforms);
+  platforms = (cl_platform_id *)malloc(sizeof(cl_platform_id) * num_platforms);
+  err = clGetPlatformIDs(num_platforms,platforms,NULL);
+  CHECK_ERROR(err);
 
-  timer_stop(0);
+  //Device
+  for(i=0;i<num_platforms;i++) {
+    // FIXME : something wrong
+    err = clGetDeviceIDs(platforms[i],dev_type,1,&dev,&num_dev);
+    if(err != CL_DEVICE_NOT_FOUND) CHECK_ERROR(err);
+    if(num_dev == 1) break;
+  }
+  if(num_dev<1) {
+    ERROR("No device");
+  }
+  
+  // Print the device name.
+  size_t name_size;
+  clGetDeviceInfo(dev, CL_DEVICE_NAME, 0, NULL, &name_size);
+  char *dev_name = (char *)malloc(name_size + 1);
+  err = clGetDeviceInfo(dev,CL_DEVICE_NAME,name_size,dev_name,NULL);
+  CHECK_ERROR(err);
+  printf("Device: %s\n",dev_name);
+  free(dev_name);
+
+  // Context
+  context = clCreateContext(NULL, 1, &dev, NULL, NULL, &err);
+  CHECK_ERROR(err);
+
+  // Command queue
+  cmd_queue = clCreateCommandQueue(context, dev, 0, &err);
+  CHECK_ERROR(err);
+
+  // Create a program
+  // TODO : Get source code in your favor
+  char * source_code=get_source_code("julia.cl");
+  
+  
+  size_t source_len=strlen(source_code);
+  program = clCreateProgramWithSource(context, 1, (const char **)&source_code, &source_len, &err);
+  CHECK_ERROR(err);
+
+  // Callback data for clBuildProgram
+  ev_bp=clCreateUserEvent(context,&err);
+  CHECK_ERROR(err);
+  bp_data_t bp_data;
+  bp_data.dev=dev;
+  bp_data.event=&ev_bp;
+
+  // Build the program.
+  err = clBuildProgram(program, 1, &dev, NULL, build_program_callback, &bp_data);
+  if (err != CL_SUCCESS) {
+    // Print the build log.
+    size_t log_size;
+    clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG,
+        0, NULL, &log_size);
+    char *log = (char *)malloc(log_size + 1);
+    clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG,
+        log_size, log, NULL);
+    fprintf(stderr,"\n");
+    fprintf(stderr,"---------- BUILD LOG ----------\n");
+    fprintf(stderr,"%s\n",log);
+    fprintf(stderr,"-------------------------------\n");
+    free(log);
+
+    CHECK_ERROR(err);
+  }
+  
+  // Buffers
+  // TODO: make and buffers
+  
+  int (*r)[h] = (int (*)[h])calloc(w * h, sizeof(int));
+  int (*g)[h] = (int (*)[h])calloc(w * h, sizeof(int));
+  int (*b)[h] = (int (*)[h])calloc(w * h, sizeof(int));
+
+  clCreateBuffer(context, CL_MEM_READ_WRITE|CL_MEM_COPY_HOST_PTR,
+      sizeof(int) * w*h, A, &err);
+  err=clEnqueueWriteBuffer(cmd_queue, R,CL_FALSE,0,w*h*sizeof(int),r,0,NULL,NULL);
+  CHECK_ERROR(err);
+  clCreateBuffer(context, CL_MEM_READ_WRITE|CL_MEM_COPY_HOST_PTR,
+      sizeof(int) * w*h, A, &err);
+  err=clEnqueueWriteBuffer(cmd_queue, R,CL_FALSE,0,w*h*sizeof(int),r,0,NULL,NULL);
+  CHECK_ERROR(err);
+
+  err=clSetKernelArg(kernel,0,sizeof(int),w);
+  CHECK_ERROR(err);
+  err=clSetKernelArg(kernel,1,sizeof(int),h);
+  CHECK_ERROR(err);
+  err=clSetKernelArg(kernel,2,sizeof(int),cRe);
+  CHECK_ERROR(err);
+  err=clSetKernelArg(kernel,3,sizeof(int),cIm);
+  CHECK_ERROR(err);
+  err=clSetKernelArg(kernel,4,sizeof(cl_mem),R);
+  CHECK_ERROR(err);
+  err=clSetKernelArg(kernel,5,sizeof(cl_mem),G);
+  CHECK_ERROR(err);
+  err=clSetKernelArg(kernel,6,sizeof(cl_mem),B);
+  CHECK_ERROR(err);
+  err=clSetKernelArg(kernel,7,sizeof(int),zoom);
+  CHECK_ERROR(err);
+  err=clSetKernelArg(kernel,8,sizeof(int),moveX);
+  CHECK_ERROR(err);
+  err=clSetKernelArg(kernel,9,sizeof(int),moveY);
+  CHECK_ERROR(err);
+  err=clSetKernelArg(kernel,10,sizeof(int),maxIterations);
+  CHECK_ERROR(err);
+
+  
+// Enqueue the kernel.
+  err=clEnqueueNDRangeKernel(cmd_queue,kernel,1,NULL,gws,lws,0,NULL,NULL);
+  CHECK_ERROR(err);
+
+  // Read the result.
+  
+  err = clEnqueueReadBuffer(cmd_queue,
+      R,
+      CL_TRUE, 0,
+      sizeof(int) * w*h,
+      r,
+      0, NULL, NULL);
+  
+  CHECK_ERROR(err);
+  err = clEnqueueReadBuffer(cmd_queue,
+      G,
+      CL_TRUE, 0,
+      sizeof(int) * w*h,
+      g,
+      0, NULL, NULL);
+  
+  CHECK_ERROR(err);
+  err = clEnqueueReadBuffer(cmd_queue,
+      B,
+      CL_TRUE, 0,
+      sizeof(int) * w*h,
+      b,
+      0, NULL, NULL);
+  
+  CHECK_ERROR(err);
+timer_stop(0);
   wtime = timer_read(0);
   printf( "\n" );
   printf( "  Time = %lf seconds.\n", wtime );
@@ -123,224 +257,21 @@ void julia(int w, int h) {
   printf( "\n" );
   printf( "  Graphics data written to \"%s\".\n\n", output_filename );
 
+
+  // Release
+  clReleaseEvent(ev_bp);
+  clReleaseMemObject(R);
+  clReleaseMemObject(G);
+  clReleaseMemObject(B);
+  clReleaseKernel(kernel);
+  clReleaseProgram(program);
+  clReleaseCommandQueue(cmd_queue);
+  clReleaseContext(context);
+  free(platforms);
+
+
   // Terminate.
   free(r);
   free(g);
   free(b);
-}
-
-
-RgbColor HSVtoRGB(unsigned h, unsigned s, unsigned v)
-{
-  RgbColor rgb;
-  unsigned char region, remainder, p, q, t;
-
-  if (s == 0)
-  {
-    rgb.r = v;
-    rgb.g = v;
-    rgb.b = v;
-    return rgb;
-  }
-
-  region = h / 43;
-  remainder = (h - (region * 43)) * 6; 
-
-  p = (v * (255 - s)) >> 8;
-  q = (v * (255 - ((s * remainder) >> 8))) >> 8;
-  t = (v * (255 - ((s * (255 - remainder)) >> 8))) >> 8;
-
-  switch (region)
-  {
-    case 0:
-      rgb.r = v; rgb.g = t; rgb.b = p;
-      break;
-    case 1:
-      rgb.r = q; rgb.g = v; rgb.b = p;
-      break;
-    case 2:
-      rgb.r = p; rgb.g = v; rgb.b = t;
-      break;
-    case 3:
-      rgb.r = p; rgb.g = q; rgb.b = v;
-      break;
-    case 4:
-      rgb.r = t; rgb.g = p; rgb.b = v;
-      break;
-    default:
-      rgb.r = v; rgb.g = p; rgb.b = q;
-      break;
-  }
-
-  return rgb;
-}
-
-/*
- * OpenCL Boilerplate by Hyunseok Cho (i@hoyajigi.com)
- * First created at 2013.10.04
- * All Rights Reserved
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <strings.h>
-#include <time.h>
-#include <math.h>
-#include <CL/cl.h>
-#include "cl_util.h"
-#include "timers.h"
-
-#define ERROR(err) fprintf(stderr, "[%s:%d] ERROR: %s\n",__FILE__,__LINE__,err);exit(EXIT_FAILURE);
-
-int main()
-{
-	/* Host data structures */
-	cl_platform_id   *platforms;
-	cl_uint          num_platforms;
-	cl_device_type   dev_type = CL_DEVICE_TYPE_DEFAULT;
-	cl_device_id     dev;
-	cl_context       context;
-	// NOTE : You might have multiple cmd_queue but whatever
-	cl_command_queue cmd_queue;
-	cl_program       program;
-	cl_kernel        kernel;
-	// TODO : define your variables
-//	cl_mem           ;
-	cl_int           err;
-	cl_uint          num_dev = 0;
-	cl_event         ev_bp;
-	
-	// TODO : 
-//	size_t lws[2]={64,4};
-//	size_t gws[2]={1024,1024};
-
-
-	int i;
-
-
-	timer_init();
-
-	srand(time(NULL));
-
-
-	// Platform
-	err = clGetPlatformIDs(0, NULL, &num_platforms);
-	CHECK_ERROR(err);
-	if(num_platforms == 0) {
-		ERROR("No OpenCl platform");
-	}
-	printf("Number of platforms: %u\n",num_platforms);
-	platforms = (cl_platform_id *)malloc(sizeof(cl_platform_id) * num_platforms);
-	err = clGetPlatformIDs(num_platforms,platforms,NULL);
-	CHECK_ERROR(err);
-
-	//Device
-	for(i=0;i<num_platforms;i++) {
-		// FIXME : something wrong
-		err = clGetDeviceIDs(platforms[i],dev_type,1,&dev,&num_dev);
-		if(err != CL_DEVICE_NOT_FOUND) CHECK_ERROR(err);
-		if(num_dev == 1) break;
-	}
-	if(num_dev<1) {
-		ERROR("No device");
-	}
-	
-	// Print the device name.
-	size_t name_size;
-	clGetDeviceInfo(dev, CL_DEVICE_NAME, 0, NULL, &name_size);
-	char *dev_name = (char *)malloc(name_size + 1);
-	err = clGetDeviceInfo(dev,CL_DEVICE_NAME,name_size,dev_name,NULL);
-	CHECK_ERROR(err);
-	printf("Device: %s\n",dev_name);
-	free(dev_name);
-
-	// Context
-	context = clCreateContext(NULL, 1, &dev, NULL, NULL, &err);
-	CHECK_ERROR(err);
-
-	// Command queue
-	cmd_queue = clCreateCommandQueue(context, dev, 0, &err);
-	CHECK_ERROR(err);
-
-	// Create a program
-	// TODO : Get source code in your favor
-	char * source_code=get_source_code("hello.cl");
-	
-	
-	size_t source_len=strlen(source_code);
-	program = clCreateProgramWithSource(context, 1, (const char **)&source_code, &source_len, &err);
-	CHECK_ERROR(err);
-
-	// Callback data for clBuildProgram
-	ev_bp=clCreateUserEvent(context,&err);
-	CHECK_ERROR(err);
-	bp_data_t bp_data;
-	bp_data.dev=dev;
-	bp_data.event=&ev_bp;
-
-	// Build the program.
-	err = clBuildProgram(program, 1, &dev, NULL, build_program_callback, &bp_data);
-	if (err != CL_SUCCESS) {
-		// Print the build log.
-		size_t log_size;
-		clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG,
-				0, NULL, &log_size);
-		char *log = (char *)malloc(log_size + 1);
-		clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG,
-				log_size, log, NULL);
-		fprintf(stderr,"\n");
-		fprintf(stderr,"---------- BUILD LOG ----------\n");
-		fprintf(stderr,"%s\n",log);
-		fprintf(stderr,"-------------------------------\n");
-		free(log);
-
-		CHECK_ERROR(err);
-	}
-	
-	// Buffers
-	// TODO: make and buffers
-	/*
-	clCreateBuffer(context, CL_MEM_READ_ONLY|CL_MEM_READ_WRITE|CL_MEM_COPY_HOST_PTR,
-			sizeof(float) * N, A, &err);
-	err=clEnqueueWriteBuffer(cmd_queue, m_array,CL_FALSE,0,size*sizeof(int),array,0,NULL,NULL);
-	*/
-	CHECK_ERROR(err);
-
-	clWaitForEvents(1,bp_data.event);
-	
-	// Kernel
-	kernel = clCreateKernel(program,"blank",&err);
-	CHECK_ERROR(err);
-
-	clFinish(cmd_queue);
-
-	// TODO : Set the arguments.
-	//err=clSetKernelArg(kernel,0,sizeof(),);
-
-	// Enqueue the kernel.
-	//err=clEnqueueNDRangeKernel(cmd_queue,kernel,1,NULL,gws,lws,0,NULL,NULL);
-	CHECK_ERROR(err);
-
-	// Read the result.
-	/*
-	err = clEnqueueReadBuffer(cmd_queue,
-			mem_C,
-			CL_TRUE, 0,
-			sizeof(float) * N,
-			C,
-			0, NULL, NULL);
-	*/
-	CHECK_ERROR(err);
-
-	// Release
-	clReleaseEvent(ev_bp);
-	//clReleaseMemObject();
-	clReleaseKernel(kernel);
-	clReleaseProgram(program);
-	clReleaseCommandQueue(cmd_queue);
-	clReleaseContext(context);
-	free(platforms);
-
-	return EXIT_SUCCESS;
 }
--- julia.cl
+++ julia.cl
@@ -1,7 +1,58 @@
-__kernel julia(int w,int h,cRe,cIm,int R,int G,int B,int zoom,int moveX,int moveY,int maxIterations)
+typedef struct {
+  unsigned int r;
+  unsigned int g;
+  unsigned int b;
+} RgbColor;
+
+RgbColor HSVtoRGB(unsigned h, unsigned s, unsigned v)
 {
-	int x;
-	int y;
+  RgbColor rgb;
+  unsigned char region, remainder, p, q, t;
+
+  if (s == 0)
+  {
+    rgb.r = v;
+    rgb.g = v;
+    rgb.b = v;
+    return rgb;
+  }
+
+  region = h / 43;
+  remainder = (h - (region * 43)) * 6; 
+
+  p = (v * (255 - s)) >> 8;
+  q = (v * (255 - ((s * remainder) >> 8))) >> 8;
+  t = (v * (255 - ((s * (255 - remainder)) >> 8))) >> 8;
+
+  switch (region)
+  {
+    case 0:
+      rgb.r = v; rgb.g = t; rgb.b = p;
+      break;
+    case 1:
+      rgb.r = q; rgb.g = v; rgb.b = p;
+      break;
+    case 2:
+      rgb.r = p; rgb.g = v; rgb.b = t;
+      break;
+    case 3:
+      rgb.r = p; rgb.g = q; rgb.b = v;
+      break;
+    case 4:
+      rgb.r = t; rgb.g = p; rgb.b = v;
+      break;
+    default:
+      rgb.r = v; rgb.g = p; rgb.b = q;
+      break;
+  }
+
+  return rgb;
+}
+
+__kernel julia(int w,int h,cRe,cIm,__global int *R,__global int *G,__global int *B,int zoom,int moveX,int moveY,int maxIterations)
+{
+	int x=get_global_id(0);
+	int y=get_global_id(1);
 	int newRe,newIm,oldRe,oldIm;
 	
     // calculate the initial real and imaginary part of z,
@@ -28,8 +79,7 @@ __kernel julia(int w,int h,cRe,cIm,int R,int G,int B,int zoom,int moveX,int move
       // use color model conversion to get rainbow palette, 
       // make brightness black if maxIterations reached
       RgbColor color = HSVtoRGB(i % 256, 255, 255 * (i < maxIterations));
-      r[y][x] = color.r;
-      g[y][x] = color.g;
-      b[y][x] = color.b;	
+      R[y*h+x] = color.r;
+      g[y*h+x] = color.g;
+      b[y*h+x] = color.b;	
 }
-
